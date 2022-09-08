67381ab4ba190f236230095c437cb4af004113d2
LeoBoro2595 <leoboro1234@gmail.com>
2022-09-08 09:06:19 -0300

    Text Spawn (No rota por la cámara)

---
+++ Assets/Trabajo Practico/GameTimer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    public Text timer;
+    public Text EstadoDeJuego;
+    float time;
+    bool isAlive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsAlive)
+        {
+            timer = Time.time * 100;
+            timer = Mathf.Floor(time) / 100;
+
+            timer.text = "" + time.ToString();
+        }
+
+
+
+        if (time > GameTime)
+        {
+            isAlive = false;
+            timer.text = timer.text;
+            EstadoDeJuego.text = "Perdiste!";
+        }
+    }
+}
---
+++ Assets/Trabajo Practico/labelMaker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class labelMaker : MonoBehaviour
+{
+
+    public GameObject textSpawnPoint;
+    public GameObject textPrefab;
+    GameObject clonText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            clonText = Instantiate(textPrefab);
+            clonText.transform.position = textSpawnPoint.transform.position;
+        }
+    }
+}
