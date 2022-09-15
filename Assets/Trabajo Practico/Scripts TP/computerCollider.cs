a1109f58c3d3bcce5ea8e6c4b294fe9a6d3893de
LeoBoro2595 <leoboro1234@gmail.com>
2022-09-15 08:52:26 -0300

    .

--- Assets/Trabajo Practico/Scripts TP/computerCollider.cs
+++ Assets/Trabajo Practico/Scripts TP/computerCollider.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class computerCollider : MonoBehaviour
 {
     [SerializeField] GameObject UIComputer;
+    public Text txt_etiquetasPegadas;
+    int etiquetasPegadas;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +27,18 @@ public class computerCollider : MonoBehaviour
         {
             UIComputer.SetActive(true);
         }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            
+        }
+
+        if (other.gameObject.CompareTag("Etiqueta"))
+        {
+            etiquetasPegadas = etiquetasPegadas + 1;
+            txt_etiquetasPegadas.text = etiquetasPegadas.ToString;
+        }
+        Debug.Log("La cantidad de etiquetas pegadas es de " + etiquetasPegadas);
     }
 
     void OnTriggerExit(Collider other)
